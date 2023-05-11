77794ae2f5a41805e666febabab3fb95e3da1885
kyounghye <kyoung00828@gmail.com>
2023-05-11 22:09:26 +0900

    code(3.6):State Practice part one Reset버튼, 분 -> 시간으로 변환해서 보여주는 기능 추가

--- kyounghye/react-for-bdginners/src/App.js
+++ kyounghye/react-for-bdginners/src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [minutes, setMinutes] = useState();
-  const [hours, sethours] = useState();
+  const [minutes, setMinutes] = useState(0);
   const handleChange = (evt) => {
     console.log(evt.target.value);
     setMinutes(evt.target.value);
+  };
+  const handleReset = () => {
+    setMinutes(0);
   };
 
   return (
@@ -22,11 +24,16 @@ function App() {
           onChange={handleChange}
         />
       </div>
-      <div>너가 입력한 {minutes}~~</div>
       <div>
         <label htmlFor="hours">Hours</label>
-        <input value={hours} id="hours" type="number" placeholder="Hours" />
+        <input
+          value={minutes / 60}
+          id="hours"
+          type="number"
+          placeholder="Hours"
+        />
       </div>
+      <button onClick={handleReset}>Reset</button>
     </>
   );
 }
