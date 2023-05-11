ae4da4bfb123087ebac27ba87c573457b1fd47bf
kyounghye <kyoung00828@gmail.com>
2023-05-11 23:52:39 +0900

    code(4.3):Prop Type prop에 타입을 지정할수 있다. 정의되지 않은 prop에 기본값을 지정할 수 있다.

--- kyounghye/react-to-practice/src/App.js
+++ kyounghye/react-to-practice/src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import { useState } from "react";
 
-const Btn = ({ title, changeValue }) => {
+//정의되지 않은 props에 대한 기본값을 설정한다.
+const Btn = ({ title, changeValue, fontSize = 16 }) => {
   return (
     <button
       onClick={changeValue}
@@ -11,12 +12,19 @@ const Btn = ({ title, changeValue }) => {
         padding: "10px 20px",
         border: 0,
         borderRadius: 10,
+        fontSize: fontSize,
       }}
     >
       {title}
     </button>
   );
 };
+
+// //props의 타입을 지정하는 방법!
+// Btn.propType = {
+//   text: PropType.string,
+//   fontSize: PropType.number,
+// };
 
 //state에 변화가 있을 때만 re-rend를 해줘!
 //const MemorizedBtn = React.memo()
@@ -30,7 +38,7 @@ function App() {
     <>
       {/*강조! props로서 넣으면 그건 절대 자동적으로 우리의 return에 들어가지
       않는다!!*/}
-      <Btn title={value} changeValue={changeValue} />
+      <Btn title={value} changeValue={changeValue} fontSize={18} />
       <Btn title="Contiue" />
     </>
   );
