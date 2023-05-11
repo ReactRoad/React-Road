fbcc1e9c964da4ac2a54d729dd51b9cd01551b8d
kyounghye <kyoung00828@gmail.com>
2023-05-11 22:54:28 +0900

    code(3.9):Final Practice and Recap 옵션 선택시 원하는 변환기가 아래 출력되도록 하는 기능 구현

--- kyounghye/react-for-bdginners/src/App.js
+++ kyounghye/react-for-bdginners/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 
-function App() {
+function MinutesToHours() {
   const [amount, setAmount] = useState(0);
   const [flipped, setFlipped] = useState(false);
   const handleChange = (evt) => {
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <>
-      <h1>Super Converter</h1>
+      <h3>Minutes To Hours</h3>
       <div>
         <label htmlFor="minutes">Minutes</label>
         <input
@@ -44,6 +44,73 @@ function App() {
       <button onClick={handleReset}>Reset</button>
       <button onClick={handleFilp}>Flip</button>
     </>
+  );
+}
+
+function KmToMiles() {
+  const [amount, setAmount] = useState(0);
+  const [flipped, setFlipped] = useState(false);
+  const handleChange = (evt) => {
+    console.log(evt.target.value);
+    setAmount(evt.target.value);
+  };
+  const handleReset = () => {
+    setAmount(0);
+  };
+  const handleFilp = () => {
+    handleReset();
+    setFlipped((current) => !current);
+  };
+
+  return (
+    <>
+      <h3>KM to Milse</h3>
+      <div>
+        <label htmlFor="minutes">Minutes</label>
+        <input
+          value={flipped ? amount : amount * 60}
+          id="minutes"
+          type="number"
+          placeholder="Minutes"
+          onChange={handleChange}
+          disabled={!flipped}
+        />
+      </div>
+      <div>
+        <label htmlFor="hours">Hours</label>
+        <input
+          value={flipped ? amount / 60 : amount}
+          id="hours"
+          type="number"
+          placeholder="Hours"
+          onChange={handleChange}
+          disabled={flipped}
+        />
+      </div>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleFilp}>Flip</button>
+    </>
+  );
+}
+
+function App() {
+  const [index, setIndex] = useState(0);
+  const onSelect = (evt) => {
+    setIndex(evt.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Super Converter</h1>
+      <select value={index} onChange={onSelect}>
+        <option value="xx">고르세요😶‍🌫️</option>
+        <option value="0">Time Converter</option>
+        <option value="1">Distance Converter</option>
+      </select>
+      {index === "xx" ? <h3>🥱</h3> : null}
+      {index === "0" ? <MinutesToHours /> : null}
+      {index === "1" ? <KmToMiles /> : null}
+    </div>
   );
 }
 
