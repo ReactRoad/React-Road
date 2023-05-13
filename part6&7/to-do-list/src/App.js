defd3054c78ee98ee8fdc97b740433fa89b538de
[SW4/1반]신성민 <ssongpig@naver.com>
2023-05-13 20:53:57 +0900

    feat: todo 작성했을 때 input을 비워주게 if문으로 작성

--- part6&7/to-do-list/src/App.js
+++ part6&7/to-do-list/src/App.js
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 function App() {
+	const [toDo, setToDo] = useState('');
+	const onChange = (event) => setToDo(event.target.value);
+	const onSubmit = (event) => {
+		event.preventDefault();
+		if (toDo === '') {
+			return;
+		}
+		setToDo('');
+	};
 	return (
 		<div>
-			<input
-				type='text'
-				placeholder='write your to do'
-			></input>
+			<form onSubmit={onSubmit}>
+				<input
+					onChange={onChange}
+					value={toDo}
+					type='text'
+					placeholder='write your to do...'
+				/>
+				<button>Add To Do</button>
+			</form>
 		</div>
 	);
 }
