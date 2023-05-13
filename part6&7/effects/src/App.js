368d30be412904b68afed568df9debf7daa6547e
[SW4/1반]신성민 <ssongpig@naver.com>
2023-05-13 18:50:34 +0900

    feat : useEffect 사용하는 이유

---
+++ part6&7/effects/src/App.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+function App() {
+	const [counter, setCounter] = useState(0);
+	const onClick = () => setCounter((prev) => prev + 1);
+	console.log('i run all the time');
+
+	useEffect(() => {
+		console.log('CALL THE API...');
+	}, []);
+	return (
+		<div>
+			<h1>{counter}</h1>
+			<button onClick={onClick}>click me</button>
+		</div>
+	);
+}
+
+export default App;
