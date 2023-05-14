c795fd0bfb7bcd39bea00e45e5f4eb873026e1eb
kyounghye <kyoung00828@gmail.com>
2023-05-14 20:04:33 +0900

    code(7.3):Movie App Part 1 데이터를 가져와서 화면에 표시하는 기능 구현!

--- kyounghye/react-movieweb/src/App.js
+++ kyounghye/react-movieweb/src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    // const response = await fetch(
+    //   `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    // );
+    // const json = await response.json();
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  console.log(movies);
+  //api에서 데이터를 가져와서 movies에 저장
+  useEffect(() => getMovies, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        {loading ? (
+          <h1> 나 로딩중이야! </h1>
+        ) : (
+          <div>
+            {movies.map((movie) => {
+              return (
+                <div key={movie.id}>
+                  <h2>{movie.title}</h2>
+                  <img src={movie.medium_cover_image} />
+                  <ul>
+                    {movie.genres.map((genres) => {
+                      return <li key={genres}>{genres}</li>;
+                    })}
+                  </ul>
+                  <p>{movie.summary}</p>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
