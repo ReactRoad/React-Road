33cd12c5a42cf68d19a3f5586c278f915de280fe
[SW4/1반]신성민 <ssongpig@naver.com>
2023-05-14 21:27:22 +0900

    feat : json movie데이터(id, img, title, summary,genres)가져와서 화면에 출력

--- part6&7/movie/src/App.js
+++ part6&7/movie/src/App.js
@@ -5,8 +5,10 @@ function Movie() {
 	const [movies, setMovies] = useState([]);
 	//async - await
 	const getMovies = async () => {
-		const json = await await fetch(
-			`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+		const json = await (
+			await fetch(
+				`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+			)
 		).json();
 		setMovies(json.data.movies);
 		setLoading(false);
@@ -25,7 +27,30 @@ function Movie() {
       */
 	}, []);
 
-	return <div>{loading ? <h1>Loading...</h1> : null}</div>;
+	return (
+		<div>
+			{loading ? (
+				<h1>Loading...</h1>
+			) : (
+				<div>
+					{movies.map((movie) => (
+						<div key={movie.id}>
+							<img src={movie.medium_cover_image} /> 
+              <h2>{movie.title}</h2>
+							<p>{movie.summary}</p>
+							<ul>
+								<li>
+									{movie.genres.map((g) => (
+										<li key={g}>{g}</li>
+									))}
+								</li>
+							</ul>
+						</div>
+					))}
+				</div>
+			)}
+		</div>
+	);
 }
 
 function App() {
