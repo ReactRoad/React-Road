9c8de79eb6e6dfdec965962f48a550bbfbee30fb
youngchae <010101chae@gmail.com>
2023-05-14 13:31:15 +0900

    code: 7.3 - Movie App 만들기 시작

--- youngchae/src/App.js
+++ youngchae/src/App.js
@@ -2,55 +2,36 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [usd, setUsd] = useState(0);
-  const [info, setInfo] = useState("");
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-  const handleUsd = (event) => setUsd(Number(event.target.value));
-  const handleSelect = (event) => {
-    setInfo(event.target.value);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
         <div>
-          <div>
-            <input
-              onChange={handleUsd}
-              value={usd}
-              type="number"
-              style={{ width: "200px" }}
-            />
-          </div>
-          <select onChange={handleSelect} value={info}>
-            <option>코인을 선택하세요.</option>
-            {coins.map((coin) => (
-              <option key={coin.id}>
-                {coin.name} ({coin.symbol}): {coin.quotes.USD.price}
-              </option>
-            ))}
-          </select>
-          <h3>
-            {info === "" ? "예산을 입력하고 코인을 선택하세요!" : null}
-            {Number(info.split(":")[1]) > usd && info !== ""
-              ? "살 수 없습니다."
-              : null}
-            {Number(info.split(":")[1]) <= usd && info !== ""
-              ? `\$${usd}로 ${Math.floor(
-                  usd / Number(info.split(":")[1])
-                )}개 살 수 있습니다.`
-              : null}
-          </h3>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       )}
     </div>
