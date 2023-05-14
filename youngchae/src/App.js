cf91298c38ff92098017b4662302b6bec452cd8b
youngchae <010101chae@gmail.com>
2023-05-14 13:04:58 +0900

    code: 7.2 - coin tracker 만들기

--- youngchae/src/App.js
+++ youngchae/src/App.js
@@ -1,36 +1,58 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((currentArray) => [toDo, ...currentArray]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [usd, setUsd] = useState(0);
+  const [info, setInfo] = useState("");
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  const handleUsd = (event) => setUsd(Number(event.target.value));
+  const handleSelect = (event) => {
+    setInfo(event.target.value);
   };
-  console.log(toDos);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <div>
+            <input
+              onChange={handleUsd}
+              value={usd}
+              type="number"
+              style={{ width: "200px" }}
+            />
+          </div>
+          <select onChange={handleSelect} value={info}>
+            <option>코인을 선택하세요.</option>
+            {coins.map((coin) => (
+              <option key={coin.id}>
+                {coin.name} ({coin.symbol}): {coin.quotes.USD.price}
+              </option>
+            ))}
+          </select>
+          <h3>
+            {info === "" ? "예산을 입력하고 코인을 선택하세요!" : null}
+            {Number(info.split(":")[1]) > usd && info !== ""
+              ? "살 수 없습니다."
+              : null}
+            {Number(info.split(":")[1]) <= usd && info !== ""
+              ? `\$${usd}로 ${Math.floor(
+                  usd / Number(info.split(":")[1])
+                )}개 살 수 있습니다.`
+              : null}
+          </h3>
+        </div>
+      )}
     </div>
   );
 }
