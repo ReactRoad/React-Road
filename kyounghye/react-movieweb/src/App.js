f7c8b1f97108b12c64069163e06bf39654a8cd3e
kyounghye <kyoung00828@gmail.com>
2023-05-14 21:01:38 +0900

    code(7.5): React Router 라우터를 사용해, 페이지 리로딩 없이 내용만 변화시켜주는 방법을 알아봤습니다~

--- kyounghye/react-movieweb/src/App.js
+++ kyounghye/react-movieweb/src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
-function App() {}
+function App() {
+  return (
+    <Router>
+      {/*한번에 하나의 라우터만 랜더링하기위해 사용*/}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
--- kyounghye/react-movieweb/src/components/Movie.js
+++ kyounghye/react-movieweb/src/components/Movie.js
@@ -1,9 +1,13 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Movie = ({ title, coverImg, genres, summary }) => {
   return (
     <div>
-      <h2>{title}</h2>
+      <h2>
+        {/* a태그 대신 Link 태그를 사용합니다~ */}
+        <Link to="/movie">{title}</Link>
+      </h2>
       <img src={coverImg} alt={title} />
       <ul>
         {genres.map((genres) => {
