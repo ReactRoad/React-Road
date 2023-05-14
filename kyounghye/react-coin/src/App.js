72c7ec66346ce34a8b98096bb564cd60337de3aa
kyounghye <kyoung00828@gmail.com>
2023-05-14 19:25:29 +0900

    code(7.2):Coin Tracker 챌린지 현재 가진 돈을 입력하면 선택한 코인을 몇 개 살 수 있는지 알려주는 기능 구현!

--- kyounghye/react-coin/src/App.js
+++ kyounghye/react-coin/src/App.js
@@ -4,6 +4,8 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoding] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [price, setPrice] = useState(0);
   useEffect(() => {
     fetch("http://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -12,19 +14,36 @@ function App() {
         setLoding(false);
       });
   }, []);
+  const handleChange = (event) => {
+    setMoney(event.target.value);
+  };
+  const onClick = (props) => {
+    console.log(props);
+  };
+  const handleChangePrice = (event) => {
+    let targetIndex = event.target.selectedIndex;
+    const selectPrice = coins[targetIndex].quotes.USD.price;
+    setPrice(selectPrice);
+    console.log(price);
+  };
   return (
     <div>
       <h1>The Coins! ({coins.length})</h1>
       {loading ? <strong>나 로딩하는중!</strong> : null}
-      <ul>
+      <input value={money} onChange={handleChange} type="number" />
+      <select onChange={handleChangePrice}>
         {coins.map((coin) => {
           return (
-            <li key={coin.id}>
+            <option key={coin.id}>
               {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
-            </li>
+            </option>
           );
         })}
-      </ul>
+      </select>
+      <button onClick={onClick}>몇 개 살수있죠?</button>
+      <div>
+        난 {money}달러 있고 {money / price}개 살 수있다!
+      </div>
     </div>
   );
 }
