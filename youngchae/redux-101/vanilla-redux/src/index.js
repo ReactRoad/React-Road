ff7ff3cef8e13e890c4d73280b0c05207a2808a1
youngchae <010101chae@gmail.com>
2023-05-17 10:19:55 +0900

    code: 1.1 - redux 설치, createStore

--- youngchae/redux-101/vanilla-redux/src/index.js
+++ youngchae/redux-101/vanilla-redux/src/index.js
@@ -1,24 +1,15 @@
+import { STATES } from "mongoose";
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (count = 0) => {
+  // count add or minus... (modify)
+  return count;
 };
 
-const handleAdd = () => {
-  count += 1;
-  updateText();
-};
+const countStore = createStore(countModifier);
 
-const handleMinus = () => {
-  count -= 1;
-  updateText();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countStore.getState());
