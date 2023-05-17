d703edfb154b61871e21b2f50ae5e8cc1b82ef2a
youngchae <010101chae@gmail.com>
2023-05-17 12:39:34 +0900

    code: 2.3 - toDo의 id가 action으로 받아온 id와 불일치하는 요소만 담은 배열을 반환

--- youngchae/redux-101/vanilla-redux/vanilla-redux-todo/src/index.js
+++ youngchae/redux-101/vanilla-redux/vanilla-redux-todo/src/index.js
@@ -23,7 +23,7 @@ const reducer = (state = [], action) => {
     case ADD_TODO:
       return [{ text: action.text, id: Date.now() }, ...state];
     case DELETE_TODO:
-      return [];
+      return state.filter((toDo) => toDo.id !== action.id);
     default:
       return state;
   }
@@ -39,7 +39,7 @@ const dispatchAddToDo = (text) => {
 
 const dispatchDeleteToDo = (e) => {
   console.log(e.target.parentNode);
-  const id = e.target.parentNode.id;
+  const id = parseInt(e.target.parentNode.id);
   store.dispatch(deleteToDo(id));
 };
 
