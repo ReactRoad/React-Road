5faf3319536106c347054dee44d065a62b57996c
[SW4/1반]신성민 <ssongpig@naver.com>
2023-05-18 22:14:26 +0900

    feat:Actions

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -5,11 +5,24 @@ const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
 //state값이 없다면 default값으로 0 지정
-const countModifier = (state = 0) => {
-	console.log(state);
-	return state;
+const countModifier = (count = 0, action) => {
+	if (action.type === 'ADD') {
+		return count + 1;
+	} else if (action.type === 'MINUS') {
+		return count - 1;
+	} else {
+		return count;
+	}
 };
 const countStore = createStore(countModifier);
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'MINUS' });
+console.log(countStore.getState());
 
 /*
 //data가 바뀌는 시점
