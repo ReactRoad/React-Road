b178ac9c24702660a21e9351b1ffb2b5d2b0b14d
[SW4/1반]신성민 <ssongpig@naver.com>
2023-05-18 21:56:36 +0900

    feat: state값이 없다면 default값으로 0 지정

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,7 +1,18 @@
+import { createStore } from 'redux';
+
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
+//state값이 없다면 default값으로 0 지정
+const countModifier = (state = 0) => {
+	console.log(state);
+	return state;
+};
+const countStore = createStore(countModifier);
+
+/*
+//data가 바뀌는 시점
 let count = 0;
 
 number.innerText = count;
@@ -19,6 +30,6 @@ const handleMinus = () => {
 	count = count - 1;
 	updateText();
 };
-
 add.addEventListener('click', handleAdd);
 minus.addEventListener('click', handleMinus);
+*/
