ce075007f535458e7859b763d188e99bc7abe4b7
[SW4/1반]신성민 <ssongpig@naver.com>
2023-05-19 15:23:31 +0900

    feat: detail screen

--- react-redux/src/components/ToDo.js
+++ react-redux/src/components/ToDo.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { actionCreators } from '../store';
+import { Link } from 'react-router-dom';
 
-function ToDo({ text, onBtnClick }) {
+function ToDo({ text, onBtnClick, id }) {
 	return (
 		<li>
-			{text}
+			<Link to={`/${id}`}>{text}</Link>
 			<button onClick={onBtnClick}>DEL</button>
 		</li>
 	);
