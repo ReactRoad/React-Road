e9ac9eff5aab15818fa2d782c664ecdecef6f700
[SW4/1반]신성민 <ssongpig@naver.com>
2023-05-19 14:34:19 +0900

    feat: Deleting To Do

--- react-redux/src/routes/Home.js
+++ react-redux/src/routes/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { actionCreators } from '../store';
+import ToDo from '../components/ToDo';
 
 function Home({ toDos, addToDo }) {
 	const [text, setText] = useState('');
@@ -24,7 +25,14 @@ function Home({ toDos, addToDo }) {
 				/>
 				<button>Add</button>
 			</form>
-			<ul>{JSON.stringify(toDos)}</ul>
+			<ul>
+				{toDos.map((toDo) => (
+					<ToDo
+						{...toDo}
+						key={toDo.id}
+					/>
+				))}
+			</ul>
 		</>
 	);
 }
