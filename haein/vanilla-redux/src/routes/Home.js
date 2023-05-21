9e3ef52f2c3ff69cc6108fb4ae498afa8864b36c
LHI0915 <gowns1998@gmail.com>
2023-05-21 16:13:05 +0900

    Feat:part3_H

---
+++ haein/vanilla-redux/src/routes/Home.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { actionCreators, addToDo } from "../store";
+import ToDo from "../components/ToDo";
+
+function Home({ toDos, addToDo }) {
+  const [text, setText] = useState("");
+
+  function onChange(e) {
+    setText(e.target.value);
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+    setText("");
+    addToDo(text);
+  }
+  return (
+    <div>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <ToDo {...toDo} key={toDo.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function mapStateToProps(state, ownProps) {
+  return { toDos: state };
+}
+
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+  };
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
